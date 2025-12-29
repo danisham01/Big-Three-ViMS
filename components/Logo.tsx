8e4344c9d7d898aa3f7cfac9b8db0a969da67c41
Danish Mustaffa <dnish.am14@gmail.com>
2025-12-30 05:53:12 +0800

    Tukar logo and Web Title

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import logoImage from '../logo.png';
 
 interface LogoProps {
   className?: string;
@@ -14,10 +15,6 @@ export const Logo: React.FC<LogoProps> = ({ className = '', size = 'md' }) => {
     xl: 'w-32 h-32'
   };
 
-  // Note: Using the provided image asset. In a production environment, 
-  // this would be a reference to a local file or a CDN URL.
-  const logoUrl = "https://files.oaiusercontent.com/file-K8kP867vU36fA6V7K4X2fG?se=2025-02-21T10%3A58%3A12Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3D46328731-5079-467f-9f76-02e0d37e3d11.webp&sig=G0B3%2BU5A4HwR2qU4U0G3Z6S7n8o9p0q1r2s3t4u5v6w%3D";
-
   return (
     <div className={`relative flex items-center justify-center ${sizeClasses[size]} ${className}`}>
       {/* Background Glow */}
@@ -25,7 +22,7 @@ export const Logo: React.FC<LogoProps> = ({ className = '', size = 'md' }) => {
       
       {/* Logo Image */}
       <img 
-        src={logoUrl} 
+        src={logoImage} 
         alt="BigThree ViMS Logo" 
         className="w-full h-full object-contain relative z-10 drop-shadow-2xl animate-in zoom-in-50 duration-700"
       />
